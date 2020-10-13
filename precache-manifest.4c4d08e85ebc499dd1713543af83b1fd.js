39423e115e92c45e54dcf9658dea473628351da4
Evgenii Kisel <kiselek.genya@gmail.com>
2020-10-13 23:29:23 +0300

    Updates

---
+++ precache-manifest.4c4d08e85ebc499dd1713543af83b1fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7244a6a5bf83220ae42b2a7736d592",
+    "url": "/for_ignat/index.html"
+  },
+  {
+    "revision": "1c8ca7813e94bc428913",
+    "url": "/for_ignat/static/css/main.78dcb8da.chunk.css"
+  },
+  {
+    "revision": "88cde142d23643d6e73f",
+    "url": "/for_ignat/static/js/2.37c999c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/for_ignat/static/js/2.37c999c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8ca7813e94bc428913",
+    "url": "/for_ignat/static/js/main.7c969966.chunk.js"
+  },
+  {
+    "revision": "7a1fd79ec4d9c7ae422d",
+    "url": "/for_ignat/static/js/runtime-main.bf6d010d.js"
+  }
+]);
